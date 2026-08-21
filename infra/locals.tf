f02a8c54540d7642e4216f322ba12e13d5bb2d04
ucoccto <edu1.ucoc@gmail.com>
2026-08-21 14:10:26 +0900

    Feat: 브론즈 레벨의 firehose의 에러 저장 경로 조정

--- infra/locals.tf
+++ infra/locals.tf
@@ -21,8 +21,8 @@ locals {
 # [실버 추가]
 # 추가되는 리소스명 정의
 locals {
-  silver_kinesis_stream_name  = "${var.project_name}-silver-kinesis"
-  silver_firehose_name        = "${var.project_name}-silver-firehose"
-  flink_application_name      = "${var.project_name}-silver-flink"
-  flink_log_group_name        = "/aws/kinesis-analysis/${var.project_name}-silver-flink"
+  silver_kinesis_stream_name = "${var.project_name}-silver-kinesis"
+  silver_firehose_name       = "${var.project_name}-silver-firehose"
+  flink_application_name     = "${var.project_name}-silver-flink"
+  flink_log_group_name       = "/aws/kinesis-analysis/${var.project_name}-silver-flink"
 }
