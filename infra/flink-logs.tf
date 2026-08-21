229534a2e736060b52ea1b240caec47799f437c9
ucoccto <edu1.ucoc@gmail.com>
2026-08-21 15:12:34 +0900

    Feat: iam role 오타 조정

--- infra/flink-logs.tf
+++ infra/flink-logs.tf
@@ -6,7 +6,7 @@ resource "aws_cloudwatch_log_group" "flink" {
 
 # 실시간 처리 =>  log stream  생성
 resource "aws_cloudwatch_log_stream" "flink" {
-  name              = local.flink_log_stream_name
+  name = local.flink_log_stream_name
   # 해당 스트림에 속할 그룹 지정
-  log_group_name    = aws_cloudwatch_log_group.flink.name
+  log_group_name = aws_cloudwatch_log_group.flink.name
 }
