a2c67e4557fda70b35eaa537a77be7c4f98e49c8
ucoccto <edu1.ucoc@gmail.com>
2026-08-25 13:40:35 +0900

    Feat: 중첩 스키마 구성

--- infra/glue-silver.tf
+++ infra/glue-silver.tf
@@ -129,19 +129,38 @@ resource "aws_glue_catalog_table" "silver" {
 
     # client 중첩 스키마
     # "client":{"ip":"200.202.139.62","user_agent":"WhitelabelApp/4.8.1 Android","device_id":"367d6a0dffb04145"}
-
+    columns {
+      name = "client"     
+      type = "struct<ip:string,user_agent:string,device_id:string>"
+    }
+    
     # request 중첩 스키마
     # "request":{"method":"GET","path":"/api/products/prd_83053","request_bytes":746}0f250bc2e713
+    columns {
+      name = "request"
+      type = "struct<method:string,path:string,request_bytes:bigint>"
+    }
 
     # response 중첩 스키마
     # "response":{"status_code":200,"latency_ms":35,"response_bytes":16686}
+    columns {
+      name = "response"
+      type = "struct<status_code:int,latency_ms:bigint,response_bytes:bigint>"
+    }
 
     # data 중첩 스키마 => 도메인별로 상이=> 모든 도메인의 키를 등록
     # "data":{"user_id":"usr_163397","session_id":"b297a82569a645bc841c","product_id":"prd_83053","category":"home","quantity":1,"unit_price":274300,"currency":"KRW","campaign":"retargeting"}
+    columns {
+      name = "data"
+      type = "struct<user_id:string,session_id:string,product_id:string,category:string,quantity:bigint,unit_price:bigint,currency:string,campaign:string,keyword:string,result_count:bigint,order_id:string,total_amount:bigint,payment_method:string,payment_result:string,transaction_id:string,customer_id:string,account_id:string,channel:string,risk_score:double,amount:bigint,merchant_id:string,merchant_category:string,authorization_result:string,destination_bank:string,destination_account_token:string,transfer_result:string,balance:bigint,auth_method:string,login_result:string,player_id:string,server_region:string,player_level:bigint,ping_ms:bigint,platform:string,match_id:string,mode:string,party_size:bigint,result:string,score:bigint,duration_seconds:bigint,item_id:string,currency_type:string,purchase_result:string,quest_id:string,reward_xp:bigint,reward_gold:bigint,plant_id:string,line_id:string,equipment_id:string,equipment_type:string,message_id:string,temperature_c:double,vibration_mm_s:double,pressure_bar:double,rpm:bigint,state:string,runtime_seconds:bigint,lot_id:string,sample_size:bigint,defect_count:bigint,quality_result:string,alarm_code:string,severity:string,acknowledged:boolean,maintenance_type:string,technician_id:string,downtime_minutes:bigint>"
+    }
 
     # 실버표기
     # "_silver":{"layer":"silver","processor":"apache-flink","schema_version":"1.0","processed_at":"2026-08-25T00:59:31.871588+00:00"}
-
+    columns {
+      name = "_silver"
+      type = "struct<layer:string,processor:string,schema_version:string,processed_at:string>"
+    }
 
   }
 }
